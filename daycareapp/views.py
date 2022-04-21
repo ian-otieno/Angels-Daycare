dd68100ccc62dcbed67e59c25db23ada524d073a
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-21 15:40:18 +0300

    create profile view for displaying staff profiles

--- daycareapp/views.py
+++ daycareapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Profile 
+from django.http import HttpResponse, Http404
+from .models import Images
 
 # Create your views here.
+
+# Image View
+
+def image(request,image_id):
+    try:
+        image = Images.objects.get(id = image_id)
+    except Images.DoesNotExist:
+        raise Http404()
+
+
+# Profile View
+
+def profile(request):
+    daycareapp = Profile.objects.all()
+    params = {
+        'daycareapp': daycareapp,
+    }
+    return render(request, 'about.html', params)
