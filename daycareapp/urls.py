d08d5c26a8fabdb02114c7ef7d427e77d502028c
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-21 15:41:51 +0300

    install cloudinary; update settings with installed apps

---
+++ daycareapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+
+urlpatterns=[
+    url('^$',views.daycareapp, name = 'daycareapp'),
+    url(r'^profile/', views.profile, name = 'profile'),
+]
